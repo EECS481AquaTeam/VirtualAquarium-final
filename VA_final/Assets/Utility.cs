cbfec30c3cf15cf125ca5d89b99d8c54e61a6acc
BradleyGoldstein <bradgold@umich.edu>
2015-12-06 17:10:01 -0500

    merged utility

--- VA_final/Assets/Utility.cs
+++ VA_final/Assets/Utility.cs
@@ -4,10 +4,16 @@ using System.Collections;
 static public class Utility {
 	private const double EQUAL_VECTORS = 0.01;	// 3d vectors are considered to be equal if the magnitude of their differences < EQUAL_VECTORS
 	
-	// Initializes the location of a fish
-	static public void InitializeFish(GameObject item, Vector3 location)
+	public const float OFFSET = 5;	// The offset of the camera in the x and y 
+	
+	public const float Z = 0;	// The value that any game objects should have in the Z direction
+	
+	static public bool kinectClickedOn = false;
+	
+	// Initializes the speed and locations of a fish
+	static public void InitializeFish(GameObject item, Vector3 location, float speed=5f)
 	{
-		item.GetComponent<ActionObject>().Initialize(location, 5f);
+		item.GetComponent<ActionObject> ().Initialize (location, speed);
 	}
 	
 	static public void MoveHelper(GameObject g1, Vector3 v1, GameObject g2, Vector3 v2)
@@ -29,11 +35,11 @@ static public class Utility {
 	}
 	
 	// Returns a 3d vector with random values in the x, y and z plains within the given range
-	static public Vector3 GetRandomVector(int range=10)
+	static public Vector3 GetRandomVector(int range=5)
 	{
-		return new Vector3(Random.Range (0, range),
-		                   Random.Range (0, range),
-		                   0);
+		return new Vector3(Random.Range (-range, range)+OFFSET,
+		                   Random.Range (-range, range)+OFFSET,
+		                   Random.Range (-range, range)+OFFSET);
 	}
 
 	static public Vector3 GetRandomVector(float xMin, float xMax, float yMin, float yMax)
